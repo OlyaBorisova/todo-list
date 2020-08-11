916166cf28592d4d1bc22f6aa70326d53ae4bb3d
OlyaBorisova <borisova-olya@bk.ru>
2020-08-11 19:18:21 +0300

    add actions on button

--- script.js
+++ script.js
@@ -1,27 +1,76 @@
-// JavaScript Document
+let themeSwitch = document.querySelector('.theme-switch');
+themeSwitch.addEventListener('change', themeSwitchFun);
 
-// Смена темы
+function themeSwitchFun() {
+	let color = themeSwitch.options[themeSwitch.selectedIndex].value;
+	let background = document.querySelector('body');
+	let toDoBackground = document.querySelector('.container');
 
-let _background = document.querySelector('html');
-let _toDoBackground = document.querySelector('.container');
-let _themeSwitch = document.querySelector('.themeSwitch');
-
-function themeSwitch() {
-	let _color = _themeSwitch.options[_themeSwitch.selectedIndex].value;
-	
-	switch (_color) {
-		case "1": 
-		_background.style.backgroundColor = '#000';
-		_toDoBackground.style.backgroundColor = '#B678FF';
-		break;
-		case "2": 
-		_background.style.backgroundColor = '#FFF';
-		_toDoBackground.style.backgroundColor = '#8fd8ff';	
-		break;
+	switch (color) {
+		case "1":
+			background.style.backgroundColor = '#000';
+			toDoBackground.style.backgroundColor = '#B678FF';
+			break;
+		case "2":
+			background.style.backgroundColor = '#FFF';
+			toDoBackground.style.backgroundColor = '#8fd8ff';
+			break;
 	}
-}
-
-_themeSwitch.addEventListener('change', themeSwitch);
+};
 
 /*--------------*/
+
+let addTaskButton = document.querySelector('.add-task__button');
+let addTaskInput = document.querySelector('.new-task');
+
+
+addTaskButton.addEventListener('click', addTaskBtnActive);
+addTaskInput.addEventListener('submit', addTaskBtnActive);
+
+function addTaskBtnActive(event) {
+
+	document.querySelector('.add-task__image').classList.toggle('add-task__image_visible');
+	document.querySelector('.add-task__image2').classList.toggle('add-task__image_visible');
+	document.querySelector('.new-task').classList.toggle('new-task_visible');
+
+	if (addTaskButton.classList.contains('add-task__button_active')) {
+		
+		function addTask() {
+			let task = document.querySelector('.new-task__input').value;
+			let taskDescription = `<div class="task">
+				<input type="checkbox" id="task-list__checkbox" class="task-list__checkbox">
+				<label for="task-list__checkbox"></label>
+				<div class="task-list__description">
+					<h2 class="task-list__heading">${task}</h2>
+					<p class="task-list__date">Сегодня 10:00 pm</p>
+				</div>	
+			</div>`;
+			document.querySelector('.task-list').innerHTML += taskDescription;	
+		} 
+	addTask ();
+	document.querySelector('.new-task__input').value = '';
+	} 	
+
+	addTaskButton.classList.toggle('add-task__button_active');
+	event.preventDefault();
+	//document.querySelector('.new-task__input').focus() // тут проблема
+}
+
+/*--------------*/
+
+addTaskButton.addEventListener('mousedown', changeAddTaskBtnSmall);
+addTaskButton.addEventListener('mouseup', changeAddTaskBtnBig);
+
+function changeAddTaskBtnSmall() {
+	addTaskButton.classList.add('add-task__button_shape');
+};
+
+function changeAddTaskBtnBig() {
+	addTaskButton.classList.remove('add-task__button_shape');
+};
+
+/*--------------*/
+
+
+
 
